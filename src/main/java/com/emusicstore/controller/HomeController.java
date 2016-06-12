f18894a32817b8de1ca14ffe71dfb73504fe13ed
Rafael <rafanercessian@gmail.com>
2016-06-11 21:28:19 -0300

    Criando Persistencia

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -1,13 +1,12 @@
 package com.emusicstore.controller;
 
-import com.emusicstore.dao.ProductDao;
 import com.emusicstore.model.Product;
+import com.emusicstore.dao.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.portlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.List;
@@ -31,7 +30,7 @@ public class HomeController {
     @RequestMapping("/productList")
     public String getProducts(Model model){
         if(products==null)
-            products=productDao.getProductList();
+            products=productDao.getAllProducts();
         model.addAttribute("products",products);
         return "productList";
     }
--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -1,66 +1,15 @@
 package com.emusicstore.dao;
 
 import com.emusicstore.model.Product;
-import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Rafael on 11/06/2016.
  */
-@Component
-public class ProductDao {
-
-    private List<Product> productList=new ArrayList<Product>();
-
-    public List<Product> getProductList(){
-        Product product1=new Product();
-        product1.setProductId("1");
-        product1.setProductName("Guitar1");
-        product1.setProductCategory("Instrument");
-        product1.setProductDescription("Fender strat guitar");
-        product1.setProductPrice(1200);
-        product1.setProductCondition("new");
-        product1.setProductStatus("Active");
-        product1.setUnitInStock(11);
-        product1.setProductManufacturer("Fender");
-        productList.add(product1);
-
-        Product product2=new Product();
-        product2.setProductId("2");
-        product2.setProductName("Record1");
-        product2.setProductCategory("Record");
-        product2.setProductDescription("Awesome mix");
-        product2.setProductPrice(25);
-        product2.setProductCondition("new");
-        product2.setProductStatus("Active");
-        product2.setUnitInStock(11);
-        product2.setProductManufacturer("EMI");
-        productList.add(product2);
-
-        Product product3=new Product();
-        product3.setProductId("3");
-        product3.setProductName("Speaker");
-        product3.setProductCategory("Speaker");
-        product3.setProductDescription("Shelf speaker");
-        product3.setProductPrice(355);
-        product3.setProductCondition("new");
-        product3.setProductStatus("Active");
-        product3.setUnitInStock(9);
-        product3.setProductManufacturer("EMI");
-        productList.add(product3);
-        return productList;
-    }
-
-    public Product getProductById(String productId) throws  IOException{
-        for (Product product:productList
-             ) {
-            if(product.getProductId().equals(productId)){
-               return product;
-            }
-        }
-        throw new IOException("No product found");
-    }
+public interface ProductDao {
+    void addProduct(Product product);
+    Product getProductById(String id);
+    List<Product> getAllProducts();
+    void deleteProduct(String id);
 }
